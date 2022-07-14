78f1aed94239cff3c7fc2245cc9328b601f6edee
syaki <syaki@KILA>
2022-07-14 11:18:55 +0800

    UPDATE NEW

--- E-Khairat/src/main/java/EkhairatDA/KhairatStaffDA.java
+++ E-Khairat/src/main/java/EkhairatDA/KhairatStaffDA.java
@@ -17,6 +17,7 @@ public class KhairatStaffDA {
 	private static final String dbURL = "jdbc:postgresql://ec2-52-72-56-59.compute-1.amazonaws.com/dd29m58g7a4tda";
 	private static final String user = "qekmfhbqusidva";
 	private static final String pass = "22cff620d0b06f17950d4f4669a1e0f11f168c04053c0a218b1d83ee130fddb9";
+	
 	public static Connection getConnection() {
         Connection con = null;
         try {
